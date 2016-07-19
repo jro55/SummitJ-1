c10399bbe775b69d140b3b5bd5579b7bea93afbe
p1zzadog <steve.moody@motili.com>
2016-07-18 19:45:31 -0600

    changed public path to /public/src

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.sendFile('/html/index.html', {root: './public'});
+    res.sendFile('/src/html/index.html', {root: './public'});
 });
 
 module.exports = router;
